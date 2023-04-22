31a675ca42c7d11ea6f04f5e0d253da30632b62a
Jonas Petzke <jonas@fam-petzke.de>
2023-04-22 20:18:47 +0200

    delete abc

--- src/autogpt_youtube/__init__.py
+++ src/autogpt_youtube/__init__.py
@@ -1,9 +1,6 @@
 """This is a template for Auto-GPT plugins."""
-import abc
 from typing import Any, Dict, List, Optional, Tuple, TypeVar, TypedDict
 import os
-
-from abstract_singleton import AbstractSingleton, Singleton
 
 from auto_gpt_plugin_template import AutoGPTPluginTemplate
 
@@ -29,7 +26,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
         # Get the YouTube API key
         self.yt_api_key = os.environ.get("YOUTUBE_API_KEY")
 
-    @abc.abstractmethod
     def can_handle_post_prompt(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_prompt method.
@@ -38,7 +34,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the post_prompt method."""
         return True
 
-    @abc.abstractmethod
     def post_prompt(self, prompt: PromptGenerator) -> PromptGenerator:
         """This method is called just after the generate_prompt is called,
             but actually before the prompt is generated.
@@ -64,7 +59,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
 
         return prompt
 
-    @abc.abstractmethod
     def can_handle_on_response(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_response method.
@@ -73,12 +67,10 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the on_response method."""
         return False
 
-    @abc.abstractmethod
     def on_response(self, response: str, *args, **kwargs) -> str:
         """This method is called when a response is received from the model."""
         pass
 
-    @abc.abstractmethod
     def can_handle_on_planning(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_planning method.
@@ -87,7 +79,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the on_planning method."""
         return False
 
-    @abc.abstractmethod
     def on_planning(
         self, prompt: PromptGenerator, messages: List[Message]
     ) -> Optional[str]:
@@ -99,7 +90,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_planning(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_planning method.
@@ -108,7 +98,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the post_planning method."""
         return False
 
-    @abc.abstractmethod
     def post_planning(self, response: str) -> str:
         """This method is called after the planning chat completion is done.
 
@@ -120,7 +109,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_pre_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the pre_instruction method.
@@ -129,7 +117,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the pre_instruction method."""
         return False
 
-    @abc.abstractmethod
     def pre_instruction(self, messages: List[Message]) -> List[Message]:
         """This method is called before the instruction chat is done.
 
@@ -141,7 +128,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_on_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the on_instruction method.
@@ -150,7 +136,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the on_instruction method."""
         return False
 
-    @abc.abstractmethod
     def on_instruction(self, messages: List[Message]) -> Optional[str]:
         """This method is called when the instruction chat is done.
 
@@ -162,7 +147,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_instruction(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_instruction method.
@@ -171,7 +155,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the post_instruction method."""
         return False
 
-    @abc.abstractmethod
     def post_instruction(self, response: str) -> str:
         """This method is called after the instruction chat is done.
 
@@ -183,7 +166,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_pre_command(self) -> bool:
         """This method is called to check that the plugin can
         handle the pre_command method.
@@ -192,7 +174,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the pre_command method."""
         return False
 
-    @abc.abstractmethod
     def pre_command(
         self, command_name: str, arguments: Dict[str, Any]
     ) -> Tuple[str, Dict[str, Any]]:
@@ -207,7 +188,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_post_command(self) -> bool:
         """This method is called to check that the plugin can
         handle the post_command method.
@@ -216,7 +196,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
             bool: True if the plugin can handle the post_command method."""
         return False
 
-    @abc.abstractmethod
     def post_command(self, command_name: str, response: str) -> str:
         """This method is called after the command is executed.
 
@@ -229,7 +208,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
         """
         pass
 
-    @abc.abstractmethod
     def can_handle_chat_completion(
         self, messages: Dict[Any, Any], model: str, temperature: float, max_tokens: int
     ) -> bool:
@@ -246,7 +224,6 @@ class AutoGPTYouTube(AutoGPTPluginTemplate):
               bool: True if the plugin can handle the chat_completion method."""
         return False
 
-    @abc.abstractmethod
     def handle_chat_completion(
         self, messages: List[Message], model: str, temperature: float, max_tokens: int
     ) -> str:
